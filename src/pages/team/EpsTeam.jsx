28f8173f65ad0e6bd2c9dd9060875f254f5cb8ea
MD Saymum Islam Siyam <si31siyam@gmail.com>
2024-11-21 23:12:04 +0600

    team details route

---
+++ src/pages/team/EpsTeam.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+function EpsTeam() {
+  const members = [
+    {
+      name: "MD Rafi",
+      id: "23024005",
+      department: "AE (Avionics)",
+    },
+  ];
+
+  return (
+    <section className="section service" id="services" aria-label="service">
+      <div className="container">
+        <p className="section-subtitle text-center">
+          -Meet the Experts Behind Our Success-
+        </p>
+        <h2 className="h2 section-title text-center">Eps Team</h2>
+        <p className="section-text text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <ul className="grid-list">
+          {members.map((member, index) => (
+            <li key={index}>
+              <div className="service-card has-after">
+                <figure className="card-icon">
+                  <img
+                    src="/assets/images/service-1.png"
+                    width={140}
+                    height={140}
+                    loading="lazy"
+                    alt={`${member.name}'s avatar`}
+                    className="img"
+                  />
+                </figure>
+                <div className="card-content">
+                  <h3 className="h3 card-title">{member.name}</h3>
+                  <p className="card-text">
+                    ID: {member.id} <br />
+                    Department: {member.department}
+                  </p>
+                  <a href="#" className="btn-link">
+                    <span className="span">Read More</span>
+                    <ion-icon name="arrow-forward-outline" aria-hidden="true" />
+                  </a>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <a href="#" className="btn btn-primary">
+          Get Started
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default EpsTeam;
--- src/routes/WebRoutes.jsx
+++ src/routes/WebRoutes.jsx
@@ -9,11 +9,21 @@ import {
 import App from "../App";
 import Home from "../pages/Home";
 import Error from "../pages/error/Error";
+import TeamDetails from "../pages/team/TeamDetails";
+import EpsTeam from "../pages/team/EpsTeam";
+import GroundStationTeam from "../pages/team/GroundStationTeam";
+import MechanicalTeam from "../pages/team/MechanicalTeam";
+import CcnTeam from "../pages/team/CcnTeam";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<Home />} />
+      <Route path="team/software" element={<TeamDetails />} />
+      <Route path="team/eps" element={<EpsTeam />} />
+      <Route path="team/ground-station" element={<GroundStationTeam />} />
+      <Route path="team/mechanical" element={<MechanicalTeam />} />
+      <Route path="team/ccn" element={<CcnTeam />} />
       <Route path="*" element={<Error />} />
     </Route>
   )
