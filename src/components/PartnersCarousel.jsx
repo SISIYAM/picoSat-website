898f3fff5171cbf2264cfcb33370043a61f9d7bc
MD Saymum Islam Siyam <si31siyam@gmail.com>
2024-12-07 14:04:11 +0600

    teams data update

--- src/components/PartnersCarousel.jsx
+++ src/components/PartnersCarousel.jsx
@@ -1,9 +1,9 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import img4 from '../assets/images/partners/img-4.png'
-import img5 from '../assets/images/partners/img-5.png'
-import img6 from '../assets/images/partners/img-6.png'
+import img4 from "../assets/images/partners/img-4.png";
+import img5 from "../assets/images/partners/bsmraau_logo.png";
+import img6 from "../assets/images/partners/a2i_logo.svg";
 
 // Import Swiper styles
 import "swiper/css";
@@ -12,38 +12,62 @@ import { Autoplay, Navigation } from "swiper/modules";
 //import "./styles.css";
 
 export default function App() {
-    return (
-
-        <section className="section service" id="teams" aria-label="service">
-            <div className="container">
-                <p className="section-subtitle text-center">
-                    -Lorem ipsum dolor-
-                </p>
-                <h2 className="h2 section-title text-center">Our Partners</h2>
-                <p className="section-text text-center">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-                <Swiper watchSlidesProgress={true} loop={true} slidesPerView={4} className="mySwiper" autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }} modules={[Autoplay, Navigation]}>
-
-                    <SwiperSlide className="flex justify-center items-center">
-                        <img src={img6} width={200} className="object-contain max-w-full max-h-full" /></SwiperSlide>
-                    <SwiperSlide className="flex justify-center items-center">
-                        <img src={img4} width={200} className="object-contain max-w-full max-h-full" /></SwiperSlide>
-                    <SwiperSlide className="flex justify-center items-center">
-                        <img src={img5} width={200} className="object-contain max-w-full max-h-full" />
-                    </SwiperSlide>
-                    <SwiperSlide className="flex justify-center items-center">
-                        <img src={img6} width={200} className="object-contain max-w-full max-h-full" />
-                    </SwiperSlide>
-                    <SwiperSlide className="flex justify-center items-center">
-                        <img src={img4} width={200} className="object-contain max-w-full max-h-full" /></SwiperSlide>
-
-                </Swiper>
-            </div>
-        </section>
-
-    );
+  return (
+    <section className="section service" id="teams" aria-label="service">
+      <div className="container">
+        <p className="section-subtitle text-center">-Lorem ipsum dolor-</p>
+        <h2 className="h2 section-title text-center">Our Partners</h2>
+        <p className="section-text text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <Swiper
+          watchSlidesProgress={true}
+          loop={true}
+          slidesPerView={4}
+          className="mySwiper"
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, Navigation]}
+        >
+          <SwiperSlide className="flex justify-center items-center">
+            <img
+              src={img6}
+              width={200}
+              className="object-contain max-w-full max-h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide className="flex justify-center items-center">
+            <img
+              src={img4}
+              width={200}
+              className="object-contain max-w-full max-h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide className="flex justify-center items-center">
+            <img
+              src={img5}
+              width={200}
+              className="object-contain max-w-full max-h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide className="flex justify-center items-center">
+            <img
+              src={img6}
+              width={200}
+              className="object-contain max-w-full max-h-full"
+            />
+          </SwiperSlide>
+          <SwiperSlide className="flex justify-center items-center">
+            <img
+              src={img4}
+              width={200}
+              className="object-contain max-w-full max-h-full"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
 }
--- src/components/TeamsSection.jsx
+++ src/components/TeamsSection.jsx
@@ -1,6 +1,49 @@
 import React from "react";
 
 function TeamsSection() {
+  const teamData = [
+    {
+      name: "Software Team",
+      slug: "software",
+      image:
+        "https://www.scnsoft.com/blog-pictures/software-development-outsourcing/sw-development-teams.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adi piscing semper turpis. Nullam eget luctlie gula and adipiscing elit.",
+    },
+    {
+      name: "Ground Station Team",
+      slug: "ground-station",
+      image:
+        "https://cdn.satsearch.com/product-images/image-fossa-systems-fossa-s-band-tuntkey-ground-station-rcags1.jpg",
+      description:
+        "Ground Station Team ensures seamless communication between satellites and the Earth station.",
+    },
+    {
+      name: "Mechanical Team",
+      slug: "mechanical",
+      image:
+        "https://img1.wsimg.com/isteam/stock/51136/:/cr=t:12.36%25,l:0%25,w:100%25,h:75.28%25/rs=w:600,h:300,cg:true",
+      description:
+        "The Mechanical Team focuses on designing robust and reliable satellite structures.",
+    },
+    {
+      name: "EPS Team",
+      slug: "eps",
+      image:
+        "https://img.freepik.com/premium-vector/electric-workers-team-transformer-service-electrical-power-lines-technician-work-box-power-repairs-teamwork-vector-illustration_229548-4006.jpg",
+      description:
+        "EPS Team works on advanced power systems to keep satellites operational.",
+    },
+    {
+      name: "CCN Team",
+      slug: "ccn",
+      image:
+        "https://coamplifi.com/wp-content/uploads/2022/08/coamplifi_effectivecomm.jpg",
+      description:
+        "CCN Team specializes in communication and networking systems for space missions.",
+    },
+  ];
+
   return (
     <section className="section service" id="teams" aria-label="service">
       <div className="container">
@@ -12,33 +55,30 @@ function TeamsSection() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
         <ul className="grid-list">
-          <li>
-            <div className="service-card has-after">
-              <figure className="card-icon">
-                <img
-                  src="./assets/images/service-1.png"
-                  width={140}
-                  height={140}
-                  loading="lazy"
-                  alt="UI/UX Creative Design"
-                  className="img"
-                />
-              </figure>
-              <div className="card-content">
-                <h3 className="h3 card-title">Software Team</h3>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet, consectetur adi piscing semper
-                  turpis. Nullam eget luctlie gula and adipiscing elit.
-                </p>
-                <a href="#" className="btn-link">
-                  <span className="span">Read More</span>
-                  <ion-icon name="arrow-forward-outline" aria-hidden="true" />
-                </a>
+          {teamData.map((team, index) => (
+            <li key={index}>
+              <div className="service-card has-after">
+                <figure className="card-icon">
+                  <img
+                    src={`${team.image}`}
+                    width={140}
+                    height={140}
+                    loading="lazy"
+                    alt={team.name}
+                    className="img"
+                  />
+                </figure>
+                <div className="card-content">
+                  <h3 className="h3 card-title">{team.name}</h3>
+                  <p className="card-text">{team.description}</p>
+                  <a href={`team/${team.slug}`} className="btn-link">
+                    <span className="span">Read More</span>
+                    <ion-icon name="arrow-forward-outline" aria-hidden="true" />
+                  </a>
+                </div>
               </div>
-            </div>
-          </li>
-
-          {/* Repeat similar service cards for other teams */}
+            </li>
+          ))}
         </ul>
         <a href="#" className="btn btn-primary">
           Get Started
